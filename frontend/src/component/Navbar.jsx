559b3b99631751933c6bf1d1c37d81ea13c665da
Sam <sam.megh0305@gmail.com>
2025-02-15 13:38:10 +0530

    somthing edited in nav bar (profile logo)

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -10,14 +10,14 @@ function Navbar() {
     <div>
     <div className='flex justify-between items-center'>
 
-       <div className="w-15 m-2 grid grid-cols-2 gap-1 justify-center shrink-0" onClick={() => navigate('/')}>
+       <div className="w-15 m-2 grid grid-cols-2 gap-1 justify-center shrink-0 cursor-default" onClick={() => navigate('/')}>
                   <img src={logo} alt="Sam Logo" className="size-7 rounded-[14px]" />
       <h3 className="text-[16px] font-bold m-0.5">
         MyChat
       </h3>
        </div>
        
-    <div className='pr-2.5 flex gap-2'>
+    <div className='pr-2.5 flex gap-2 justify-center items-center'>
      <Link to='/setting' className='btn btn-sm pt-0.5 pr-3 pl-3 pb-0.5 items-center rounded-xl'>
      <button className='text-[15px] flex gap-1 cursor-pointer'>
     <Settings size={18} className='mt-0.5' />
@@ -36,6 +36,9 @@ function Navbar() {
     <LogOut size={18} />
     <span className='hidden sm:inline'>Logout</span>
       </button>
+      <div className="cursor-default overflow-hidden rounded-[14px] scale-130" onClick={() => navigate('/')}>
+                  <img src={isAuth.profileimage} alt="Profile Logo" className="size-7 scale-130" />
+       </div>
 </>)
 }
     </div>
