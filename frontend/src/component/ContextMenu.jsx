a174d3c05162115790cc7191d8d2d0b9a32d525b
Sam <sam.megh0305@gmail.com>
2025-02-25 22:47:17 +0530

    working from here

--- frontend/src/component/ContextMenu.jsx
+++ frontend/src/component/ContextMenu.jsx
@@ -1,138 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-import { useChatStore } from "../files/useChatStore";
-import MessageSkeleton from "../skelton/MessageSkeleton";
-import Chatheader from "./Chatheader";
-import Messageinput from "./messageinput";
-import { checkAuthStore } from "../files/checkAuthFile";
-import defaultimg from '../pages/logoimg/default-avatar.png';
-import ContextMenu from "./ContextMenu";
+import React from 'react'
+import { useState } from 'react';
 
-const ChatContainer = () => {
-  const { messages, getMessages, isMessageLoading, selectedUser, setToMessage, unSetToMessage, isImage } = useChatStore();
-  const { isAuth } = checkAuthStore();
-  const [showMenu, setShowMenu] = useState(null);
-  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
-  const messageEndRef = useRef(null);
-  useEffect(() => {
-    getMessages(selectedUser._id);
-    setToMessage();
-    return () => unSetToMessage();
-  }, [selectedUser._id, getMessages, setToMessage, unSetToMessage]);
-
-  useEffect(() => {
-    if (messageEndRef.current && messages) {
-      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [messages]);
-  if (isMessageLoading) return (
-    <div className="flex flex-1 flex-col overflow-auto">
-      <Chatheader />
-      <MessageSkeleton />
-      <Messageinput />
-    </div>
-  );
-
-  const showTime = (time) => {
-    const [hr, min] = time.split("T")[1].split(":").slice(0, 2);
-    const hrint = parseInt(hr)
-    const period = (hrint > 12) ? "PM" : "AM"
-    let newTime = [hr, min].join(":")
-    newTime = [newTime, period].join(" ")
-    return newTime
-  }
-
-  const showDate = (date) => {
-    const [year, monthno, day] = date.split("T")[0].split("-").slice(0, 3)
-    const monthnoint = parseInt(monthno)
-    const monthname = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    const month = monthname[monthnoint]
-    const newDate = [day, month, year].join("-")
-    return newDate
-  }
+function ContextMenu() {
+    const list=['Reply','Delete'];
   return (
-    <div className="flex-1 flex flex-col overflow-auto">
-      <Chatheader />
-
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message, index) => {
-          const prevMessage = messages[index - 1]; // Get previous message
-          const prevDate = prevMessage ? showDate(prevMessage.createdAt) : null;
-          const curDate = showDate(message.createdAt);
-          return (
-            <div key={index}>
-              {index === 0 || prevDate != curDate &&
-                
-                  <div className='rounded-xl bg-primary/90 mx-auto w-fit px-3 left select-none'>
-                    <p className='text-base-100'>
-                      {showDate(message.createdAt)}</p>
-                  </div>
-              }
-              <div
-                key={message._id}
-                className={`chat ${message.senderid === isAuth._id ? "chat-end " : "chat-start"}`}
-                ref={messageEndRef}
-
-              >
-
-                <div className=" chat-image avatar">
-                  <div className="size-10 rounded-full border">
-                    <img
-                      src={
-                        message.senderid === isAuth._id
-                          ? isAuth.profileimage || defaultimg
-                          : selectedUser.profileimage || defaultimg
-                      }
-                      alt="profile pic"
-                    />
-                  </div>
-                </div>
-                <div className="chat-header mb-1">
-                  <time className="text-xs opacity-50 ml-1">
-                    {showTime(message.createdAt)}
-                  </time>
-                </div>
-                <div className={`chat-bubble flex flex-col ${message.senderid === isAuth._id ? " bg-primary text-primary-content" : ""}`}
-                  onContextMenu={(e) => {
-                    e.preventDefault();
-                    setMenuPosition({ x: e.clientX + 4, y: e.clientY + 4 });
-                    setShowMenu(message._id);
-                  }}>
-                  {message.image && (
-                    <img
-                      src={message.image}
-                      alt="Attachment"
-                      className="sm:max-w-[200px] rounded-md mb-2"
-                    />
-                  )}
-                  {message.message && <p>{message.message}</p>}
-                </div>
-
-              </div>
+    <div>
+        <div className="w-fit bg-base-300 rounded-xl z-2 p-4 px-7 flex flex-col gap-2 cursor-default">
+        {list.map((item,index)=>(
+                    <span 
+                    key={index}
+                    className={`text-center w-15 `}>
+                    {item}
+                    <hr className={`mt-1 border-t-2 transition-all duration-200 ease-in-out`}/>
+                    </span>
+                    
+                ))}
             </div>
-          )
-        }
-
-
-        )}
-        {isImage && <>
-          <div className="skeleton h-16 w-[200px] float-right" />
-        </>
-        }
-
-        <div className={`${showMenu ? "absolute duration-100" : "hidden"}`} style={{ top: menuPosition.y, left: menuPosition.x }}>
-          <ContextMenu />
-        </div>
-
-
-        {
-          window.addEventListener('click', () => {
-            setShowMenu(null)
-          })
-        }
-      </div>
-
-      <Messageinput />
+        
     </div>
-  );
-};
-export default ChatContainer;
+  )
+}
+
+export default ContextMenu
