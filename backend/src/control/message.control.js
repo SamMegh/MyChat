97a6649bf8c6727b511434dd9d035b29d980f87a
Sam <sam.megh0305@gmail.com>
2025-02-26 23:41:31 +0530

    backend is ready for delete rout

--- backend/src/control/message.control.js
+++ backend/src/control/message.control.js
@@ -1,37 +1,37 @@
 import User from "../model/user.model.js";
 import Message from "../model/message.model.js";
 import { getReceiverSocketId, io } from "../lib/socketio.lib.js";
-import cloudinary from '../lib/couldinary.lib.js';
-import axios from 'axios';          
-import fs from 'fs';                
-import FormData from 'form-data';  
+import DeletedMessage from "../model/deletedMessage.model.js";
+import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
 
 
-const BOT_TOKEN =process.env.BOT_TOKEN; 
-const CHAT_ID = process.env.CHAT_ID; 
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
 export const getusers = async (req, res) => {
-     try {
-        let loginuser= req.user._id;
-           let users = await User.find({ _id: { $ne: loginuser } }).select('-password');
-           res.status(200).json(users);
-     } catch (error) {
+    try {
+        let loginuser = req.user._id;
+        let users = await User.find({ _id: { $ne: loginuser } }).select('-password');
+        res.status(200).json(users);
+    } catch (error) {
         res.status(500).json({ message: 'Internal server error' + error });
-        
-     }
+
     }
+}
 
 export const getmessages = async (req, res) => {
     try {
-        let {id:receiverid} = req.params;
-        let loginuser= req.user._id;
-        let messages= await Message.find({
-            $or:[
+        let { id: receiverid } = req.params;
+        let loginuser = req.user._id;
+        let messages = await Message.find({
+            $or: [
                 {
-                    senderid:loginuser,receiverid:receiverid
+                    senderid: loginuser, receiverid: receiverid
                 },
                 {
-                    senderid:receiverid,receiverid:loginuser
+                    senderid: receiverid, receiverid: loginuser
                 }
             ]
         });
@@ -45,32 +45,32 @@ export const getmessages = async (req, res) => {
 
 export const sendmessage = async (req, res) => {
     try {
-        let {id:receiverid} = req.params;
-        let senderid= req.user._id;
-        let {message, image} = req.body;
+        let { id: receiverid } = req.params;
+        let senderid = req.user._id;
+        let { message, image } = req.body;
         let imgurl;
-        if(image){
-            imgurl= await imagedatabase(image);
+        if (image) {
+            imgurl = await imagedatabase(image);
         }
         let newmessage = new Message({
             senderid,
             receiverid,
             message,
-            image:imgurl
+            image: imgurl
         });
         await newmessage.save();
 
-const receiverSocketid=getReceiverSocketId(receiverid);
-if(receiverSocketid){
-    io.to(receiverSocketid).emit("newone",newmessage)
-}
+        const receiverSocketid = getReceiverSocketId(receiverid);
+        if (receiverSocketid) {
+            io.to(receiverSocketid).emit("newone", newmessage)
+        }
 
         res.status(200).json(newmessage);
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' + error });
     }
 }
-const imagedatabase= async(base64Image)=>{
+const imagedatabase = async (base64Image) => {
     const cleanBase64 = base64Image.replace(/^data:image\/\w+;base64,/, "");
     const imageBuffer = Buffer.from(cleanBase64, 'base64');
     const filePath = './image.jpg';  // Temporary file path
@@ -91,7 +91,7 @@ const imagedatabase= async(base64Image)=>{
                 headers: formData.getHeaders(),
             }
         );
-        
+
         // Extract file ID
         const fileId = response.data.result?.photo?.pop()?.file_id;
         if (!fileId) {
@@ -112,4 +112,28 @@ const imagedatabase= async(base64Image)=>{
         // Clean up the temporary image file
         fs.unlinkSync(filePath);
     }
-}    
+}
+
+export const deletemessage = async (req, res) => {
+    try {
+        let { id: messageid } = req.params;
+        const message = await Message.findById(messageid);
+        if (!message) {
+            return res.status(404).json({ message: "Message not found" });
+        }
+        const deletedMessage = new DeletedMessage({
+            messageid: message._id,
+            senderid: message.senderid,
+            receiverid: message.receiverid,
+            message: message.message,
+            image: message.image,
+            oldCreatedAt: message.createdAt 
+        });
+        await deletedMessage.save();
+        await Message.deleteOne({ _id: messageid });
+        res.status(200).json({ message: "Message deleted" });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' + error });
+    }
+}
