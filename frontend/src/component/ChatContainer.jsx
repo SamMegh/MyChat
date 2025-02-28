400c6098a7c42b2fb4f61561f6bb11e06c8a0982
Ankit <sam.megh0305@gmail.com>
2025-02-28 13:43:50 +0530

    context menu postion changed

--- frontend/src/component/ChatContainer.jsx
+++ frontend/src/component/ChatContainer.jsx
@@ -151,19 +151,7 @@ const ChatContainer = () => {
         }
 
         <div className={`${showMenu ? "absolute duration-100" : "hidden"}`} style={{ top: menuPosition.y, left: menuPosition.x }}>
-          <div className="w-[116px] bg-base-300 rounded-xl z-2 p-4 px-7 flex flex-col gap-2 cursor-default">
-        {list.map((item,index)=>(
-                    <span 
-                    key={index}
-                    className={`text-center w-15 `}
-                    onClick={()=>handlecontaxtmenu(item)}
-                    >
-                    {item}
-                    <hr className={`mt-1 border-t-2 transition-all duration-200 ease-in-out`}/>
-                    </span>
-                    
-                ))}
-            </div>
+          <ContextMenu/>
         </div>
 
 
